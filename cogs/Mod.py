e2a33dc71bbdb920d943bf197d642ae9d5ed0be7
zackprogrammer <63200308+zackprogrammer@users.noreply.github.com>
2021-06-29 15:31:36 -0400

    The phantom coder will fix the intidation. this is a bug

--- cogs/Mod.py
+++ cogs/Mod.py
@@ -8,9 +8,16 @@ class Moderation(commands.Cog):
     @commands.Cog.Listener()
     async def on_ready(self):
         print("To infintie and BEYYYYYYYYYOND")
-    @bot.command()
+    @commands.command()
     async def ping(self, ctx):
         await ctx.send('pong')    
+    @commands.command()
+    async def kick(self, ctx, user: discord.Member, *, reason=None):
+        if not reason:
+            await user.kick("User has been kicked of no reason")
+        else:
+            await user.kick(reason=reason)
+            await ctx.send(f"User got gnomed out of the server for {reason}. And you are a lunatic for doing that") 
 
 def setup(client):
     client.add_cog(Moderation(client))        
